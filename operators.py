0b6a045d2eed87f9cc24d4f5cc948965b25f642b
vaidehi <vaidehivyas2007@gmail.com>
2026-06-09 15:56:44 +0530

    second

---
+++ operators.py
@@ -0,0 +1,17 @@
+'''
+operators: symbol that perform to spicific operation
+
+'''
+a = 10
+b = 100
+
+print ( a + b )
+print ( a - b )
+print ( a // b )
+print ( a ** b )
+print ( a  % b )
+
+
+print ( a is not b )
+print (a == b )
+print (a != b )
