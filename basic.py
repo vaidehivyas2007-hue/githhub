932e97535077abbd70ff9dfbb918a70c5f9db478
vaidehi <vaidehivyas2007@gmail.com>
2026-06-09 12:37:36 +0530

    python projects

---
+++ basic.py
@@ -0,0 +1,20 @@
+Python 3.14.5 (tags/v3.14.5:5607950, May 10 2026, 10:43:50) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print ('my name is vaidehi")
+...        
+SyntaxError: unterminated string literal (detected at line 1)
+>>> print ( " my name is vaidehi")
+...        
+ my name is vaidehi
+>>> print ( '25+25+25+25+25-96")
+...         
+SyntaxError: unterminated string literal (detected at line 1)
+>>> print ('52+52+52+52-96*85')
+...         
+52+52+52+52-96*85
+>>> print ('hello')
+...         
+hello
+>>> print ( '25+23')
+...         
+25+23
